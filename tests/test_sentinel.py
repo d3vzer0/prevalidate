ac3b3ad3a32c6dbb2df4fbc7ed86322d74f1e607
Joey Dreijer <jdreijer@d3vzer0.com>
2023-05-25 12:49:06 +0200

    Update unit test

--- tests/test_sentinel.py
+++ tests/test_sentinel.py
@@ -7,21 +7,28 @@ from prevalidate.sentinel.main import Workspace
 import requests
 import os
 
+BASE_PATH = os.path.dirname(__file__)
+
 
 def test_detections_to_markdown():
-    detections = SentinelDetections.from_yaml('tests/test_data/sentinel/detections', workspace=None)
+    detections_dir = os.path.join(BASE_PATH, 'test_data/sentinel/detections')
+    detections = SentinelDetections.from_yaml(detections_dir, workspace=None)
     detections.to_markdown('../test')
 
 
 def test_validate():
-    workspace = Workspace.from_file('./schema.json')
-    detections = SentinelDetections.from_yaml('tests/test_data/sentinel/detections', workspace=workspace)
+    schema_file = os.path.join(BASE_PATH, 'test_data/sentinel/schema.json')
+    detections_dir = os.path.join(BASE_PATH, 'test_data/sentinel/detections')
+
+    workspace = Workspace.from_file(schema_file)
+    detections = SentinelDetections.from_yaml(detections_dir, workspace=workspace)
     issues = [issue for issue in detections.validate()]
-    assert len(issues) == 1
+    assert len(issues) == 2
 
 
 def test_workspace_file():
-    workspace = Workspace.from_file('./schema.json')
+    schema_file = os.path.join(BASE_PATH, 'test_data/sentinel/schema.json')
+    workspace = Workspace.from_file(schema_file)
     assert workspace.functions and workspace.tables
 
 
